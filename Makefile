3d09d6d0c1c9a1bf25a44b48777768f3f7029440
Brendan Kerrigan <kerriganb@ainfosec.com>
2016-05-09 14:13:26 -0400

    [build] Addressed some concerns about the build system changes associated with OSX experimental support pull request.
    Signed-off-by: Brendan Kerrigan <kerriganb@ainfosec.com>

--- Makefile
+++ Makefile
@@ -64,9 +64,6 @@ include ./common/common_subdir.mk
 .PHONY: linux_load
 .PHONY: linux_unload
 .PHONY: linux_clean
-.PHONY: osx_load
-.PHONY: osx_unload
-.PHONY: osx_clean
 .PHONY: load
 .PHONY: unload
 .PHONY: start
@@ -94,16 +91,6 @@ linux_unload: force
 	make clean
 
 linux_clean: linux_unload
-
-osx_load: force
-	@cd bfdrivers/src/arch/osx; \
-	echo osx_load
-
-osx_unload: force
-	@cd bfdrivers/src/arch/osx; \
-	echo osx_unload
-
-osx_clean: osx_unload
 
 load: force
 	@cd bfm/bin/native; \
--- bfdrivers/src/arch/osx/fixup-kext-perms.sh
+++ bfdrivers/src/arch/osx/fixup-kext-perms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo chown -R root /Users/brendank310/Library/Developer/Xcode/DerivedData/bareflank-*/Build/Products/Debug/bareflank.kext
-sudo chgrp -R wheel /Users/brendank310/Library/Developer/Xcode/DerivedData/bareflank-*/Build/Products/Debug/bareflank.kext
+sudo chown -R root $HOME/Library/Developer/Xcode/DerivedData/bareflank-*/Build/Products/Debug/bareflank.kext
+sudo chgrp -R wheel $HOME/Library/Developer/Xcode/DerivedData/bareflank-*/Build/Products/Debug/bareflank.kext
 
