b956335232acd43eedefbef2df3ec9bd8fe5ca03
Rian Quinn <rianquinn@gmail.com>
2016-09-08 07:11:26 -0600

    Fix endl (#190)
    * Remove std::endl
    
    This patch removes uses of std::endl
    
    [ISSUE]: https://github.com/Bareflank/hypervisor/issues/180
    
    Signed-off-by: “Rian <“rianquinn@gmail.com”>
    
    * Remove std::endl
    
    [ISSUE]: https://github.com/Bareflank/hypervisor/issues/180
    
    Signed-off-by: “Rian <“rianquinn@gmail.com”>

--- include/debug.h
+++ include/debug.h
@@ -53,10 +53,10 @@ void output_to_vcpu(uint64_t vcpuid, T func)
     func();
 }
 
-/// This macro is a shortcut for std::endl
+/// Newline macro
 ///
 #ifndef bfendl
-#define bfendl std::endl
+#define bfendl '\n'
 #endif
 
 /// This macro is a shortcut for std::cout that adds some text and color.
