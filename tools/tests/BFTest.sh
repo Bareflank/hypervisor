d32c83914178175c529cd7f6bd17c3851861d9c9
Joshua Zepf <joze4229@colorado.edu>
2017-02-15 11:42:54 -0700

    Removed check_status function and added EXIT handler

--- tools/tests/BFTest.sh
+++ tools/tests/BFTest.sh
@@ -20,83 +20,82 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-check_status()
+set -e
+report_error()
 {
-    if [ $? -ne 0 ] ; then
-        echo BFTest FAILED > /dev/ttyS1
-        sleep 31
-    fi
+    echo "Ubuntu EXIT" > /dev/ttyS1
 }
 
 test_hypervisor()
 {
-    sudo make > /dev/ttyS1 ; check_status
-    sudo make test > /dev/ttyS1 ; check_status
-    sudo ./tools/tests/test_hypervisor.sh > /dev/ttyS1 ; check_status
+    sudo make > /dev/ttyS1
+    sudo make test > /dev/ttyS1
+    sudo ./tools/tests/test_hypervisor.sh > /dev/ttyS1
     echo Hypervisor Done > /dev/ttyS1
 }
 
 test_extended_apis()
 {
-    git clone https://github.com/Bareflank/extended_apis > /dev/ttyS1 ; check_status
-    sudo ./configure -m extended_apis/bin/extended_apis.modules > /dev/ttyS1 ; check_status
-    sudo make > /dev/ttyS1 ; check_status
-    sudo make test > /dev/ttyS1 ; check_status
-    sudo ./extended_apis/tests/test_extended_apis.sh > /dev/ttyS1 ; check_status
+    git clone https://github.com/Bareflank/extended_apis > /dev/ttyS1
+    sudo ./configure -m extended_apis/bin/extended_apis.modules > /dev/ttyS1
+    sudo make > /dev/ttyS1
+    sudo make test > /dev/ttyS1
+    sudo ./extended_apis/tests/test_extended_apis.sh > /dev/ttyS1
     echo EAPIs Done > /dev/ttyS1
 }
 
 test_hyperkernel()
 {
-    git clone https://github.com/Bareflank/hyperkernel > /dev/ttyS1 ; check_status
-    sudo ./configure -m hyperkernel/bin/hyperkernel.modules > /dev/ttyS1 ; check_status
-    sudo make > /dev/ttyS1 ; check_status
-    sudo make test > /dev/ttyS1 ; check_status
-    sudo ./hyperkernel/tests/test_hyperkernel.sh > /dev/ttyS1 ; check_status
+    git clone https://github.com/Bareflank/hyperkernel > /dev/ttyS1
+    sudo ./configure -m hyperkernel/bin/hyperkernel.modules > /dev/ttyS1
+    sudo make > /dev/ttyS1
+    sudo make test > /dev/ttyS1
+    sudo ./hyperkernel/tests/test_hyperkernel.sh > /dev/ttyS1
     echo Hyperkernel Done > /dev/ttyS1
 }
 
 test_vpid()
 {
-    git clone https://github.com/Bareflank/hypervisor_example_vpid > /dev/ttyS1 ; check_status
-    sudo ./configure -m hypervisor_example_vpid/bin/vpid.modules -e hypervisor_example_vpid >/dev/ttyS1 ; check_status
-    sudo make > /dev/ttyS1 ; check_status
-    sudo make driver_load > /dev/ttyS1 ; check_status
-    sudo make quick > /dev/ttyS1 ; check_status
-    sudo make status > /dev/ttyS1 ; check_status
-    sudo make dump > /dev/ttyS1 ; check_status
-    sudo make stop > /dev/ttyS1 ; check_status
-    sudo make driver_unload > /dev/ttyS1 ; check_status
+    git clone https://github.com/Bareflank/hypervisor_example_vpid > /dev/ttyS1
+    sudo ./configure -m hypervisor_example_vpid/bin/vpid.modules -e hypervisor_example_vpid >/dev/ttyS1
+    sudo make > /dev/ttyS1
+    sudo make driver_load > /dev/ttyS1
+    sudo make quick > /dev/ttyS1
+    sudo make status > /dev/ttyS1
+    sudo make dump > /dev/ttyS1
+    sudo make stop > /dev/ttyS1
+    sudo make driver_unload > /dev/ttyS1
     echo VPID Done > /dev/ttyS1
 }
 
 test_cpuid()
 {
-    git clone https://github.com/Bareflank/hypervisor_example_cpuidcount > /dev/ttyS1 ; check_status
-    sudo ./configure -m hypervisor_example_cpuidcount/bin/cpuidcount.modules > /dev/ttyS1 ; check_status
-    sudo make > /dev/ttyS1 ; check_status
-    sudo make driver_load > /dev/ttyS1 ; check_status
-    sudo make quick > /dev/ttyS1 ; check_status
+    git clone https://github.com/Bareflank/hypervisor_example_cpuidcount > /dev/ttyS1
+    sudo ./configure -m hypervisor_example_cpuidcount/bin/cpuidcount.modules > /dev/ttyS1
+    sudo make > /dev/ttyS1
+    sudo make driver_load > /dev/ttyS1
+    sudo make quick > /dev/ttyS1
     sudo ARGS="string json '{\"get\":\"count\"}'" make vmcall > /dev/ttyS1 ; check_status
-    sudo make stop > /dev/ttyS1 ; check_status
-    sudo make driver_unload > /dev/ttyS1 ; check_status
+    sudo make stop > /dev/ttyS1
+    sudo make driver_unload > /dev/ttyS1
     echo CPUID Done > /dev/ttyS1
 }
 
 test_hook()
 {
-    git clone https://github.com/Bareflank/extended_apis_example_hook src_extended_apis_example_hook > /dev/ttyS1 ; check_status
-    sudo ./configure -m src_extended_apis_example_hook/bin/hook.modules > /dev/ttyS1 ; check_status
-    sudo make >/dev/ttyS1 ; check_status
-    sudo make driver_load > /dev/ttyS1 ; check_status
-    sudo make quick > /dev/ttyS1 ; check_status
-    sudo ./makefiles/src_extended_apis_example_hook/app/bin/native/hook > /dev/ttyS1 ; check_status
-    sudo make stop > /dev/ttyS1 ; check_status
-    sudo make driver_unload > /dev/ttyS1 ; check_status
+    git clone https://github.com/Bareflank/extended_apis_example_hook src_extended_apis_example_hook > /dev/ttyS1
+    sudo ./configure -m src_extended_apis_example_hook/bin/hook.modules > /dev/ttyS1
+    sudo make >/dev/ttyS1
+    sudo make driver_load > /dev/ttyS1
+    sudo make quick > /dev/ttyS1
+    sudo ./makefiles/src_extended_apis_example_hook/app/bin/native/hook > /dev/ttyS1
+    sudo make stop > /dev/ttyS1
+    sudo make driver_unload > /dev/ttyS1
     echo Hook Done > /dev/ttyS1
 }
 
-# Tests assume you are in the ~/hypervisor directory and have run your setup_XXX script 
+trap report_error EXIT
+# Tests assume you are in the ~/hypervisor directory and have run your setup_XXX script
 test_hypervisor
 test_extended_apis
 test_hyperkernel
@@ -105,3 +104,4 @@ test_cpuid
 test_hook
 
 echo BFTest SUCCESS > /dev/ttyS1
+sleep 60;
