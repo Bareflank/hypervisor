4e304a0027296772d48f799988f61e604ea5fddb
Rian Quinn <rianquinn@gmail.com>
2017-03-23 11:31:36 -0700

    Missing Windows VMCall Logic
    The VMCall ASM was missing the MS64 bit version of this
    logic
    
    Signed-off-by: Rian Quinn <“rianquinn@gmail.com”>

--- common/vmcall_intel_x64.asm
+++ common/vmcall_intel_x64.asm
@@ -24,6 +24,87 @@ default rel
 
 section .text
 
+%ifdef MS64
+
+; ------------------------------------------------------------------------------
+; MS64
+; ------------------------------------------------------------------------------
+
+global vmcall:function
+vmcall:
+
+    push rbx
+    push rdi
+    push rsi
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov rdi, rcx
+
+    mov rax, [rdi + 0x00]
+    mov rdx, [rdi + 0x08]
+    mov rcx, [rdi + 0x10]
+    mov rbx, [rdi + 0x18]
+    mov rsi, [rdi + 0x20]
+    mov r8,  [rdi + 0x28]
+    mov r9,  [rdi + 0x30]
+    mov r10, [rdi + 0x38]
+    mov r11, [rdi + 0x40]
+    mov r12, [rdi + 0x48]
+    mov r13, [rdi + 0x50]
+    mov r14, [rdi + 0x58]
+    mov r15, [rdi + 0x60]
+
+    vmcall
+
+    mov [rdi + 0x60], r15
+    mov [rdi + 0x58], r14
+    mov [rdi + 0x50], r13
+    mov [rdi + 0x48], r12
+    mov [rdi + 0x40], r11
+    mov [rdi + 0x38], r10
+    mov [rdi + 0x30], r9
+    mov [rdi + 0x28], r8
+    mov [rdi + 0x20], rsi
+    mov [rdi + 0x18], rbx
+    mov [rdi + 0x10], rcx
+    mov [rdi + 0x08], rdx
+
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rsi
+    pop rdi
+    pop rbx
+
+    ret
+
+global vmcall_event:function
+vmcall_event:
+
+    push rdi
+    mov rdi, rcx
+
+    mov rax, [rdi + 0x00]
+    mov rdx, [rdi + 0x08]
+    mov rcx, [rdi + 0x10]
+
+    vmcall
+
+    mov [rdi + 0x08], rdx
+
+    pop rdi
+    ret
+
+%else
+
+; ------------------------------------------------------------------------------
+; SYSV
+; ------------------------------------------------------------------------------
+
 global vmcall:function
 vmcall:
 
@@ -82,3 +163,5 @@ vmcall_event:
     mov [rdi + 0x08], rdx
 
     ret
+
+%endif
