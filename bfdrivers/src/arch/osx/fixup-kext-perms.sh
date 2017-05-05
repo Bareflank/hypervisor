b00e9b71dd305f945c568fe78b0a328e016bb8e5
Rian Quinn <rianquinn@gmail.com>
2017-05-04 20:41:03 -0600

    initial super build script
    Signed-off-by: Rian Quinn <“rianquinn@gmail.com”>

--- bfdrivers/src/arch/osx/fixup-kext-perms.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo chown -R root $HOME/Library/Developer/Xcode/DerivedData/bareflank-*/Build/Products/Debug/bareflank.kext
-sudo chgrp -R wheel $HOME/Library/Developer/Xcode/DerivedData/bareflank-*/Build/Products/Debug/bareflank.kext
-
--- tools/scripts/build_windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd /D C:\ewdk\
-call LaunchBuildEnv.cmd
-
-cd /D %1
-msbuild /p:Configuration=Debug /p:Platform=x64 /p:TargetVersion=%3 /p:OutDir=%2\outdir\ /p:IntDir=%2\intdir\ Makefile.sln
