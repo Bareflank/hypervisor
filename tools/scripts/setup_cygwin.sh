6705c09e31adef5e40c7e1f912c6ef837b391893
Randshot <randshot@unknownrealm.eu>
2017-05-28 20:15:15 +0200

    Added cmake to cygwin setup
    Signed-off-by: Randshot <randshot@unknownrealm.eu>

--- tools/scripts/setup_cygwin.sh
+++ tools/scripts/setup_cygwin.sh
@@ -42,7 +42,7 @@ parse_arguments $@
 # ------------------------------------------------------------------------------
 
 install_common_packages() {
-    setup-x86_64.exe -q --wait -P make,gcc-core,gcc-g++,diffutils,libgmp-devel,libmpfr-devel,libmpc-devel,flex,bison,nasm,texinfo,unzip,git-completion,bash-completion,patch,ncurses,libncurses-devel,clang,clang++,libiconv-devel,curl,wget
+    setup-x86_64.exe -q --wait -P make,gcc-core,gcc-g++,diffutils,libgmp-devel,libmpfr-devel,libmpc-devel,flex,bison,nasm,texinfo,unzip,git-completion,bash-completion,patch,ncurses,libncurses-devel,clang,clang++,libiconv-devel,curl,wget,cmake
 }
 
 setup_ewdk() {
