1c50ba51f4d22c3388f071fefdbd55015e0bcc96
Randshot <randshot@unknownrealm.eu>
2017-05-28 19:55:15 +0200

    Fix: `make driver_load` wouldn't compile on a drive different than C
    Signed-off-by: Randshot <randshot@unknownrealm.eu>

--- tools/scripts/build_windows.bat
+++ tools/scripts/build_windows.bat
@@ -1,7 +1,7 @@
 @echo off
 
-cd C:\ewdk\
+cd /D C:\ewdk\
 call LaunchBuildEnv.cmd
 
-cd %1
+cd /D %1
 msbuild /p:Configuration=Debug /p:Platform=x64 /p:TargetVersion=%3 /p:OutDir=%2\outdir\ /p:IntDir=%2\intdir\ Makefile.sln
