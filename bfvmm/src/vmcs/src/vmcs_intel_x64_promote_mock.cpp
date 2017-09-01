feac655d5a0682904e4a81e080a31f93b7dac1e4
Rian Quinn <rianquinn@gmail.com>
2017-09-01 14:48:53 -0600

    missing VMCS unit tests (#481)
    Signed-off-by: Rian Quinn <“rianquinn@gmail.com”>

--- bfvmm/src/vmcs/src/vmcs_intel_x64_promote_mock.cpp
+++ bfvmm/src/vmcs/src/vmcs_intel_x64_promote_mock.cpp
@@ -19,17 +19,11 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-#include <bfgsl.h>
-#include <bfdebug.h>
 #include <bftypes.h>
-
 #include <vmcs/vmcs_intel_x64_promote.h>
 
 extern "C" void
 vmcs_promote(uintptr_t state_save) noexcept
 {
     bfignored(state_save);
-
-    std::cerr << __BFFUNC__ << " called" << '\n';
-    abort();
 }
