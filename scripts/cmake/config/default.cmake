982cf02a0b04e42697810de590ede1774bfd2545
JaredWright <wrightj@ainfosec.com>
2017-10-12 10:17:26 -0400

    Changes from discussion about Pull Request #508
    1) Fixed EP_UPDATE_DISCONNECTED to set as a directory property
    2) Put checks for nasm back into the global project cmake file
    3) Changed comment to signify future use of CMAKE_HOST_SYSTEM_NAME
    
    This commit will be squashed back into a single commit when pull
    request #508 is ready to be merged.

--- scripts/cmake/config/default.cmake
+++ scripts/cmake/config/default.cmake
@@ -1,4 +1,4 @@
-# All configurable build properties for Bareflank are defined here, set to 
+# All configurable build properties for Bareflank are defined here, set to
 # their default values.
 
 # ------------------------------------------------------------------------------
@@ -8,11 +8,11 @@
 set(CMAKE_BUILD_TYPE "Release"
     CACHE STRING
     "The type of build"
-    )
+)
 set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
     "Release"
     "Debug"
-    )
+)
 
 # ------------------------------------------------------------------------------
 # Build attributes
@@ -20,53 +20,53 @@ set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
 
 
 set(BUILD_SHARED_LIBS OFF
-	CACHE BOOL
+    CACHE BOOL
     "Build libraries as shared libraries"
-	)
+)
 
 set(BUILD_STATIC_LIBS ON
-	CACHE BOOL
-	"Build libraries as static libraries"
-	)
+    CACHE BOOL
+    "Build libraries as static libraries"
+)
 
 # ------------------------------------------------------------------------------
 # Developer Features
 # ------------------------------------------------------------------------------
 
 set(ENABLE_UNITTESTING OFF
-	CACHE BOOL
-	"Enable unit testing"
-	)
+    CACHE BOOL
+    "Enable unit testing"
+)
 
 set(ENABLE_ASAN OFF
     CACHE BOOL
     "Enable clang AddressSanitizer"
-    )
+)
 
 set(ENABLE_USAN OFF
     CACHE BOOL
     "Enable clang UndefinedBehaviorSanitizer"
-    )
+)
 
 set(ENABLE_COVERITY OFF
     CACHE BOOL
     "Enable coverity static analysis"
-    )
+)
 
 set(ENABLE_TIDY OFF
     CACHE BOOL
     "Enable clang-tidy"
-    )
+)
 
 set(ENABLE_ASTYLE OFF
     CACHE BOOL
     "Enable astyle formatting"
-    )
+)
 
 set(ENABLE_DEPEND_UPDATES OFF
     CACHE BOOL
     "Check dependencies for updates on every build"
-    )
+)
 
 # ------------------------------------------------------------------------------
 # Toolchains
@@ -75,70 +75,70 @@ set(ENABLE_DEPEND_UPDATES OFF
 set(TOOLCHAIN_PATH_BINUTILS ${BF_DEFAULT_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building GNU binutils"
-    )
+)
 
 set(TOOLCHAIN_PATH_CATCH ${BF_DEFAULT_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building catch"
-    )
+)
 
 set(TOOLCHAIN_PATH_GSL ${BF_DEFAULT_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building C++ guidelines support library"
-    )
+)
 
 set(TOOLCHAIN_PATH_HIPPOMOCKS ${BF_DEFAULT_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building hippomocks"
-    )
+)
 
 set(TOOLCHAIN_PATH_JSON ${BF_DEFAULT_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building JSON"
-    )
+)
 
 set(TOOLCHAIN_PATH_LIBCXX ${BF_DEFAULT_VMM_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building libc++"
-    )
+)
 
 set(TOOLCHAIN_PATH_LIBCXXABI ${BF_DEFAULT_VMM_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building libc++abi"
-    )
+)
 
 set(TOOLCHAIN_PATH_NEWLIB ${BF_DEFAULT_VMM_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building newlib"
-    )
+)
 
 set(TOOLCHAIN_PATH_BFDRIVER ${BF_DEFAULT_KERNEL_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building bfdriver"
-    )
+)
 
 set(TOOLCHAIN_PATH_BFELF_LOADER ${BF_DEFAULT_VMM_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building bfelf_loader"
-    )
+)
 
 set(TOOLCHAIN_PATH_BFM ${BF_DEFAULT_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building bfm"
-    )
+)
 
 set(TOOLCHAIN_PATH_BFSDK ${BF_DEFAULT_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building bfsdk"
-    )
+)
 
 set(TOOLCHAIN_PATH_BFSYSROOT ${BF_DEFAULT_VMM_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building bfsysroot"
-    )
+)
 
 set(TOOLCHAIN_PATH_BFVMM ${BF_DEFAULT_VMM_TOOLCHAIN_FILE}
     CACHE PATH
     "Path to a cmake toolchain file for building bfvmm"
-    )
+)
 
--- scripts/cmake/global_vars.cmake
+++ scripts/cmake/global_vars.cmake
@@ -11,61 +11,61 @@
 # ------------------------------------------------------------------------------
 
 set(BF_SOURCE_DIR ${CMAKE_SOURCE_DIR}
-	CACHE INTERNAL 
-	"Top-level source directory"
-	)
+    CACHE INTERNAL
+    "Top-level source directory"
+)
 
 set(BF_SCRIPTS_DIR "${BF_SOURCE_DIR}/scripts"
-	CACHE INTERNAL 
+    CACHE INTERNAL
     "Scripts directory"
-	)
+)
 
 set(BF_CONFIG_DIR "${BF_SCRIPTS_DIR}/cmake/config"
-	CACHE INTERNAL 
+    CACHE INTERNAL
     "Cmake build configurations directory"
-	)
+)
 
 set(BF_DEPENDS_DIR "${BF_SCRIPTS_DIR}/cmake/depends"
-	CACHE INTERNAL 
+    CACHE INTERNAL
     "Cmake external dependencies directory"
-	)
+)
 
 set(BF_FLAGS_DIR "${BF_SCRIPTS_DIR}/cmake/flags"
-	CACHE INTERNAL 
+    CACHE INTERNAL
     "Cmake compiler flags directory"
-	)
+)
 
 set(BF_TARGETS_DIR "${BF_SCRIPTS_DIR}/cmake/targets"
-	CACHE INTERNAL 
+    CACHE INTERNAL
     "Cmake custom build targets directory"
-	)
+)
 
 set(BF_TOOLCHAIN_DIR "${BF_SCRIPTS_DIR}/cmake/toolchain"
-	CACHE INTERNAL 
+    CACHE INTERNAL
     "Cmake toolchain files directory"
-	)
+)
 
 # ------------------------------------------------------------------------------
 # Build tree structure
 # ------------------------------------------------------------------------------
 
 set(BF_BUILD_DIR ${CMAKE_BINARY_DIR}
-	CACHE INTERNAL 
-	"Top-level build directory"
-	)
+    CACHE INTERNAL
+    "Top-level build directory"
+)
 
 set(BF_BUILD_DEPENDS_DIR ${BF_BUILD_DIR}/depends
-	CACHE INTERNAL 
-	"Build directory for external dependencies"
-	)
+    CACHE INTERNAL
+    "Build directory for external dependencies"
+)
 
 # TODO: The path to a directory named "bfprefix" is currently dictated
 # by the env.sh script. Remove that requirement and make this path configurable
 # set(BF_BUILD_INSTALL_DIR ${BF_BUILD_DIR}/install
 set(BF_BUILD_INSTALL_DIR ${BF_BUILD_DIR}/bfprefix
-	CACHE INTERNAL 
+    CACHE INTERNAL
     "Intermediate build installation directory"
-	)
+)
 
 # ------------------------------------------------------------------------------
 # Default toolchains
@@ -74,15 +74,15 @@ set(BF_BUILD_INSTALL_DIR ${BF_BUILD_DIR}/bfprefix
 set(BF_DEFAULT_TOOLCHAIN_FILE "${BF_TOOLCHAIN_DIR}/default.cmake"
     CACHE INTERNAL
     "Path to the default cmake toolchain file for building userspace tools"
-    )
+)
 
 set(BF_DEFAULT_KERNEL_TOOLCHAIN_FILE "${BF_TOOLCHAIN_DIR}/default_kernel.cmake"
     CACHE INTERNAL
     "Path to the default cmake toolchain file for building kernel/driver tools"
-    )
+)
 
 set(BF_DEFAULT_VMM_TOOLCHAIN_FILE "${BF_TOOLCHAIN_DIR}/default_vmm.cmake"
     CACHE INTERNAL
     "Path to the default cmake toolchain file for building vmm tools"
-    )
+)
 
