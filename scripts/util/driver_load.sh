dcaee5abe51f1fb112b2512a12ccbabcc81eb6d9
Rian Quinn <rianquinn@gmail.com>
2018-08-17 12:49:43 -0600

    Fix Windows Driver Issue
    The new VS and WDK have different paths and features turned on
    that break our current build system. The following addresses these
    issues

--- scripts/util/driver_load.sh
+++ scripts/util/driver_load.sh
@@ -22,13 +22,33 @@
 
 # $1 == CMAKE_SOURCE_DIR
 
+certmgr_10_0_00000_0="/cygdrive/c/Program Files (x86)/Windows Kits/10/bin/x64/certmgr"
+certmgr_10_0_17134_0="/cygdrive/c/Program Files (x86)/Windows Kits/10/bin/10.0.17134.0/x64/certmgr"
+
+find_certmgr() {
+
+    if [[ -f $certmgr_10_0_17134_0 ]]; then
+        certmgr=$certmgr_10_0_17134_0
+        return
+    fi
+
+    if [[ -f $certmgr_10_0_00000_0 ]]; then
+        certmgr=$certmgr_10_0_00000_0
+        return
+    fi
+
+    >&2 echo "ERROR: failed to find certmgr"
+    exit 1
+}
+
 case $(uname -s) in
 CYGWIN_NT*)
+    find_certmgr
     cd $1/src/platform/windows
-    >&2 /cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/10/bin/x64/certmgr /add x64/Debug/bareflank.cer /s /r localMachine root
-    >&2 /cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/10/bin/x64/certmgr /add x64/Debug/bareflank.cer /s /r localMachine trustedpublisher
+    >&2 eval "'$certmgr' /add x64/Release/bareflank.cer /s /r localMachine root"
+    >&2 eval "'$certmgr' /add x64/Release/bareflank.cer /s /r localMachine trustedpublisher"
     >&2 /cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/10/Tools/x64/devcon remove "ROOT\bareflank"
-    >&2 /cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/10/Tools/x64/devcon install x64/Debug/bareflank/bareflank.inf "ROOT\bareflank"
+    >&2 /cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/10/Tools/x64/devcon install x64/Release/bareflank/bareflank.inf "ROOT\bareflank"
     ;;
 Linux)
     cd $1/src/platform/linux
