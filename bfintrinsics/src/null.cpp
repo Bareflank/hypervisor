04f14cda53a4537fd2bf50453080b8545b9f85c5
Rian Quinn <rianquinn@gmail.com>
2018-12-03 07:00:20 -0700

    fix compile issue with VS

---
+++ bfintrinsics/src/null.cpp
@@ -0,0 +1,28 @@
+//
+// Bareflank Hypervisor
+// Copyright (C) 2015 Assured Information Security, Inc.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+#include <windows.h>
+
+void
+do_nothing_function()
+{
+    // This function is here to ensure that a CPP file is compiled as part
+    // of the build for the intrinsics. Without this, some compilers get mad
+    // like Visual Studio as they attempt to link in libraries that assume
+    // a C or CPP file was compiled
+}
