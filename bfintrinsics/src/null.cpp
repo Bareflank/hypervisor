6ee23b88318496797bab9921dac5eaa7003d4be7
Rian Quinn <rianquinn@gmail.com>
2018-12-03 08:15:53 -0700

    Update null.cpp

--- bfintrinsics/src/null.cpp
+++ bfintrinsics/src/null.cpp
@@ -16,8 +16,6 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-#include <windows.h>
-
 void
 do_nothing_function()
 {
